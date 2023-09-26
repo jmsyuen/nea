8d731111271b729290f02c1cd25afbeabc2b783b
yinhp <richardyin369@gmail.com>
2023-09-26 12:17:29 +0100

    test

--- pokersim.py
+++ pokersim.py
@@ -10,7 +10,7 @@ class new_round():
     self._suit = ("hearts","diamonds","spades","clubs")
     self._value = [2,3,4,5,6,7,8,9,10,"j","q","k",1]
     self._cards = dict(for suit in self._suit)
-    for i in self._suit:
+    #for i in self._suit:
       
   def Pick_card(self):
     randsuit = random.randint(1,4)
---
+++ testing.py
@@ -0,0 +1,14 @@
+
+
+letters = ["a", "b" , "c", "d"]
+numbers = [1,2,3,4]
+dictionary = dict()
+
+for l in letters:
+    for n in numbers:
+        dictionary[l] = n
+
+print(dictionary)
+#
+# dictionary hearts:[1,2,3,4], diamond:[1,2,3,4]
+# dictonary = {k:v for (k,v) in }
