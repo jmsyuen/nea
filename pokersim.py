73207fb4245755d2b8fa6a5cab3277f00b38d87c
jmsyuen <144684713+jmsyuen@users.noreply.github.com>
2023-10-03 12:05:07 +0100

    Update pokersim.py

--- pokersim.py
+++ pokersim.py
@@ -8,13 +8,13 @@ class new_round():
   def __init__(self, players):
     self._players = players
     self._suit = ("hearts","diamonds","spades","clubs")
-    self._value = [2,3,4,5,6,7,8,9,10,"j","q","k",1]
-    self._cards = dict(for suit in self._suit)
-    #for i in self._suit:
+    self._deck = dict()
+    for suit in self._suit:
+      self._deck[suit] = [value for value in range(14)]
       
   def Pick_card(self):
     randsuit = random.randint(1,4)
-    randvalue = random.randint(1,len(self._value))
+    randvalue = random.randint(1,len(#))
     print(randsuit, randvalue)
     
 
