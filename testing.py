dcda30afd8a317d22f640eab22b445cb71e87ce9
jmsyuen <144684713+jmsyuen@users.noreply.github.com>
2023-10-03 11:50:53 +0100

    corrected dictionary bug

--- testing.py
+++ testing.py
@@ -1,27 +1,31 @@
 
 
-letters = ["a", "b" , "c", "d"]
-numbers = [1,2,3,4]
+letters = ["s", "d" , "c", "h"]
+numbers = [2,3,5,7]
 dictionary = dict()
-
 for l in letters:
-    dictionary[l] = numbers
+  dictionary[l] = [1,2,3,4,5,6]
 
 #dictonary2 = {k:v for (k,v) in letters,numbers}
+print(dictionary)
 
-#
+dictionary["d"].pop(1)
+
+
 # dictionary hearts:[1,2,3,4], diamond:[1,2,3,4]
 # dictonary = {k:v for (k,v) in }
-print(dictionary["a"].remove(2))
-#dictionary.pop("a")
-print(dictionary["a"])
+#print(dictionary["d"].remove(2))
+#dictionary["d"].pop(1)
+print(dictionary["d"])
+print(dictionary)
 
-
-for k in dictionary:
-  dictionary[k] = set(dictionary[k])
+#for k in dictionary:
+#  dictionary[k] = set(dictionary[k])
 
 
 # for a in d.values()
 # a.remove(4)       except value error
-print(dictionary.values())
-print(dictionary.items())
+#print(dictionary.values())
+#print(dictionary.items())
+
+deck = [[]]
