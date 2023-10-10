772be603078628f1dec3c58380514b372039fd6e
jmsyuen <144684713+jmsyuen@users.noreply.github.com>
2023-10-10 12:05:18 +0100

    Update pokersim.py
    # find a way to return the popped value at l23
    # find a way to have a deck that doesn't reset every time function is called
    # find a way to call one instance and refer back to that instance without resetting

--- pokersim.py
+++ pokersim.py
@@ -2,20 +2,32 @@ import random
 
 # ranked
 # testsave
-
+# randint is inclusive of both limits
+# range is inclusive of first value only eg. 1,14 is 1-13
 
 class new_round():
   def __init__(self, players):
     self._players = players
-    self._suit = ("hearts","diamonds","spades","clubs")
-    self._deck = dict()
-    for suit in self._suit:
-      self._deck[suit] = [value for value in range(14)]
-      
-  def Pick_card(self):
-    randsuit = random.randint(1,4)
-    randvalue = random.randint(1,len(#))
-    print(randsuit, randvalue)
+    self.suit = ("hearts","diamonds","spades","clubs")
+    self.deck = dict()
+    for suit in self.suit:
+      self.deck[suit] = [value for value in range(1,14)]
+      # 1 - 13
+  # def New_deck(self):
     
+  def Pick_card(self):
+    randsuit = self.suit[random.randint(0,3)]
+    randvalue = random.randint(1,len(self.deck[randsuit]))
+    # remaining cards in suit
+    print(randsuit, randvalue)
+    return self.deck[randsuit].pop(randvalue - 1)
+    
+  def Deck(self):
+    return self.deck
 
-new_round(5).Pick_card()
+
+print(new_round(5).Pick_card())
+print(new_round(5).Deck()
+# find a way to return the popped value at l23
+# find a way to have a deck that doesn't reset every time function is called
+# find a way to call one instance and refer back to that instance without resetting
