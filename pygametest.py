e375f85f0456fa0b9aa85b655d5e723e0e7eaf51
Richard Yin Windows <richardyin369@gmail.com>
2023-12-20 19:42:44 +0000

    positions for top 5 cards, list of players on side, two big cards at bottom, added buttons for fold call raise decided to use more buttons instead of sliders in next commit

--- pygametest.py
+++ pygametest.py
@@ -1,57 +1,98 @@
 import pygame
 import sys
 import random
+import time
 
 pygame.init()
 
 # Constants
+#origin at top left
 WIDTH, HEIGHT = 414, 896 #logical point resolution of iphone 11
 WHITE = (255, 255, 255)
 GREEN = (0, 128, 0)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
-CARD_WIDTH, CARD_HEIGHT = 70, 92
-LARGE_CARD_WIDTH, LARGE_CARD_HEIGHT = 120, 164
+GREY = (220, 220, 220)
+#small card size 50x72px, big 100x144px
+#includes padding of 20px around right and bottom sides
+SMALL_CARD_WIDTH, SMALL_CARD_HEIGHT = 70, 92  
+BIG_CARD_WIDTH, BIG_CARD_HEIGHT = 120, 164  
+#buttons
+button_width, button_height = 40, 20
+button_padding = 20
+button_font_size = 15
+button_font_colour = WHITE
+button_fill_colour = BLACK
+button_border_colour = WHITE
+button_border_width = 1
+
 
 # Initialize Pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Poker")
 
 # Load card images
-card_images = []  # https://code.google.com/archive/p/vector-playing-cards/
-for suit in ["hearts", "diamonds", "spades", "clubs"]:
-  for value in range(2, 15):
-      card_images.append(pygame.image.load(f"small_cards/{suit}.{value}.png"))
+small_card_images = []  # https://code.google.com/archive/p/vector-playing-cards/
+big_card_images = []
+back = [] #small, big
 
-# Function to draw cards
-def draw_cards(cards, x, y):
-  for i, card in enumerate(cards):
-      screen.blit(card_images[card - 1], (x + i * CARD_WIDTH, y))
+for suit in ("hearts", "diamonds", "spades", "clubs"):
+  for value in range(2, 15):
+    small_card_images.append(pygame.image.load(f"small_cards/{suit}.{value}.png"))  
+    big_card_images.append(pygame.image.load(f"big_cards/{suit}.{value}.png"))
+
+#map index 2-14 to 0-12, add offset of 13 for each suit
+
+back.append(pygame.image.load(f"small_cards/back.png"))
+back.append(pygame.image.load(f"big_cards/back.png"))
+
+
+#two big cards at bottom left
+#maybe big public cards at top 
+#list of 9 other players with cards on right side
+# reveal cards in succession by moving down 5px and flipping and up 5px again
+
+
+
+
 
 # Function to draw buttons
-def draw_button(text, x, y, width, height, color, hover_color, action):
+def draw_button(text, x, y, width, height, colour, action):
   mouse = pygame.mouse.get_pos()
   click = pygame.mouse.get_pressed()
-
+  hover_colour = GREY
+  #check if hover
   if x + width > mouse[0] > x and y + height > mouse[1] > y:
-    pygame.draw.rect(screen, hover_color, (x, y, width, height))
+    pygame.draw.rect(screen, hover_colour, (x, y, width, height))
     if click[0] == 1:
       action()
-    else:
-      pygame.draw.rect(screen, color, (x, y, width, height))
+  else:
+    pygame.draw.rect(screen, colour, (x, y, width, height))
 
-    small_text = pygame.font.SysFont("courier", 20)
-    text_surface, text_rect = text_objects(text, small_text)
-    text_rect.center = ((x + (width / 2)), (y + (height / 2)))
-    screen.blit(text_surface, text_rect)
+  #white border
+  border_width = 1
+  pygame.draw.rect(screen, (255, 255, 255), (x, y, width, height), border_width)
+
+  #add text
+  small_text = pygame.font.SysFont("courier", 15)
+  text_surface, text_rect = text_objects(text, small_text)
+  text_rect.center = ((x + (width / 2)), (y + (height / 2)))
+  screen.blit(text_surface, text_rect)
+
+
 
 # Function to create text objects
 def text_objects(text, font):
   text_surface = font.render(text, True, WHITE)
   return text_surface, text_surface.get_rect()
 
+def out():
+  print("out")
+
+
+
 # Main game loop
-def game_loop():
+def draw_game():
   cards_on_table = [random.randint(1, 52) for _ in range(5)]
   player_hand = [random.randint(1, 52) for _ in range(2)]
 
@@ -61,19 +102,51 @@ def game_loop():
         pygame.quit()
         sys.exit()
 
-    screen.fill(BLACK)
+    screen.fill(BLACK)  #background
+    #5 small cards at top
+    for i in range(5):
+      screen.blit(back[0], (40 + i * SMALL_CARD_WIDTH, 20))
+    
+    #list of players on left for right handed players on mobile
+    for i in range(6):
+      screen.blit(back[0], (20 , 112 + i * SMALL_CARD_HEIGHT))
+      screen.blit(back[0], (90 , 112 + i * SMALL_CARD_HEIGHT))
+    
+    #big cards bottom left
+    screen.blit(back[1], (20, HEIGHT - 162))
+    screen.blit(back[1], (20 + BIG_CARD_WIDTH, HEIGHT - 162))
 
+    
     # Draw cards on the table
-    draw_cards(cards_on_table, 50, 250)
+    #draw_cards(cards_on_table, 50, 250)
 
     # Draw player's hand
-    draw_cards(player_hand, 50, 450)
+    #draw_cards(player_hand, 50, 450)
 
-    # Draw buttons
-    draw_button("Fold", 200, 500, 100, 50, RED, (255, 0, 0), game_loop)
-    draw_button("Call", 350, 500, 100, 50, RED, (255, 0, 0), game_loop)
-    draw_button("Raise", 500, 500, 100, 50, RED, (255, 0, 0), game_loop)
 
+
+    draw_button("Fold", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 80, 60, 30, BLACK, out)
+    draw_button("Check", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 140, 60, 30, BLACK, out)
+    draw_button("Raise", WIDTH - 80, HEIGHT - 200, 60, 30, BLACK, out)
+    #instead of sliders add more buttons to raise pot by (chip values just like a real table)
+
+    draw_button("Fold", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 80, 60, 30, BLACK, out)
+    draw_button("Call", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 140, 60, 30, BLACK, out)
+    draw_button("Raise", WIDTH - 80, HEIGHT - 200, 60, 30, BLACK, out)
+
+    draw_button("Fold", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 80, 60, 30, BLACK, out)
+    draw_button("All In", 10 + BIG_CARD_WIDTH*2 , HEIGHT - 140, 60, 30, BLACK, out)
+
+
+    #update display
     pygame.display.flip()
 
-game_loop()
+draw_game()
+
+
+'''
+# Function to draw cards
+def draw_cards(cards, x, y):
+  for i, card in enumerate(cards):
+      screen.blit(card_images[card - 1], (x + i * CARD_WIDTH, y))
+'''
